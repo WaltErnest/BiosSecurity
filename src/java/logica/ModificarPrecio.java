94d9818f0d9027853903bb11b81af1f22dbd9635
desquitin <desquitin@DESARROLLO-DE.dynatech.intranet>
2017-06-09 15:40:10 -0300

    Modificar precios - emprezado, es para que me quede actualizado cuando baje el proyecto en casa.

---
+++ src/java/logica/ModificarPrecio.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+/**
+ *
+ * @author desquitin
+ */
+// queda a modo de prueba nada mas
+public class ModificarPrecio {
+    
+    public static void main(String[] args) throws IOException {
+
+        File TextFile = new File("C:/Users/desquitin/Documents/NetBeansProjects/BiosSecurity/Resources/ListaDePreciosTest.txt");
+        FileWriter TextOut = new FileWriter(TextFile, true);
+        
+        TextOut.write("Prueba de Grabación \r\n");
+        TextOut.close();
+    }
+}
