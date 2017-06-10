165d094bb99c49cb61e56a118e5c298803151b8b
Mathias <Mathias@192.168.1.102>
2017-06-10 12:43:58 -0300

    altaCliente y modificarCliente agregados

--- src/java/persistencia/PersistenciaCliente.java
+++ src/java/persistencia/PersistenciaCliente.java
@@ -6,6 +6,7 @@
 package persistencia;
 
 import entidades.*;
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -55,6 +56,70 @@ public class PersistenciaCliente implements IPersistenciaCliente {
             }
             if (resultado != null) {
                 resultado.close();
+            }
+            if (cnn != null) {
+                Conexion.Desconectar(cnn);
+            }
+        }
+    }
+    
+    public void altaCliente(Cliente pCliente) throws ClassNotFoundException, SQLException, MiExcepcion{
+        Connection cnn = null;
+        CallableStatement consulta = null;
+        
+        try {
+            cnn = Conexion.Conectar();
+            consulta = cnn.prepareCall("{ CALL altaCliente(?, ?, ?, ?, ?, ?) }");
+            
+            consulta.setLong(1, pCliente.getCedula());
+            consulta.setString(2, pCliente.getNombre());
+            consulta.setString(3, pCliente.getDireccionCobro());
+            consulta.setString(4, pCliente.getBarrioDirCobro());
+            consulta.setLong(5, pCliente.getTelefono());
+            consulta.registerOutParameter(6, java.sql.Types.VARCHAR);
+            
+            consulta.executeUpdate();
+            
+            String error = consulta.getString(6);
+            
+            if (error != null) {
+                throw new MiExcepcion("Error en dar de alta al cliente" + pCliente.getCedula() + ": " + error);
+            }           
+        } finally {
+            if (consulta != null) {
+                consulta.close();
+            }
+            if (cnn != null) {
+                Conexion.Desconectar(cnn);
+            }
+        }        
+    }
+    
+    public void modificarCliente(Cliente pCliente) throws ClassNotFoundException, SQLException, MiExcepcion {
+        Connection cnn = null;
+        CallableStatement consulta = null;
+        
+        try {
+            cnn = Conexion.Conectar();
+            consulta = cnn.prepareCall("{ CALL modificarCliente(?, ?, ?, ?, ?, ?)}");
+            
+            consulta.setLong(1, pCliente.getCedula());
+            consulta.setString(2, pCliente.getNombre());
+            consulta.setString(3, pCliente.getDireccionCobro());
+            consulta.setString(4, pCliente.getBarrioDirCobro());
+            consulta.setLong(5, pCliente.getTelefono());
+            consulta.registerOutParameter(6, java.sql.Types.VARCHAR);
+            
+            consulta.executeUpdate();
+            
+            String error = consulta.getString(6);
+            
+            if (error != null) {
+                throw new MiExcepcion("Error al modificar el cliente" + pCliente.getCedula() + ": " + error);
+            }     
+        } finally {
+            if (consulta != null) {
+                consulta.close();
             }
             if (cnn != null) {
                 Conexion.Desconectar(cnn);
