edc26775c9c26b89139310afde49bfd6ef10895c
Mathias <Mathias@192.168.1.102>
2017-06-10 15:39:20 -0300

    agregué buscarPropiedad() a interfaz

---
+++ src/java/persistencia/IPersistenciaPropiedad.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package persistencia;
+
+import entidades.*;
+import java.sql.SQLException;
+import miexcepcion.MiExcepcion;
+/**
+ *
+ * @author Mathias
+ */
+public interface IPersistenciaPropiedad {
+    
+    Propiedad buscarPropiedad(int pNumeroPropiedad, long pCedulaDueno) throws ClassNotFoundException, SQLException, MiExcepcion;
+}
