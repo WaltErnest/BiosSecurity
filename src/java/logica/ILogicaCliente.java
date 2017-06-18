cfe90159e441ad53f4de283ec3b53f77a6302fe5
Mathias <Mathias@192.168.1.102>
2017-06-18 20:29:35 -0300

    agregados buscar, alta y modificar

---
+++ src/java/logica/ILogicaCliente.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+import compartidos.beans.entidades.Cliente;
+import compartidos.beans.excepciones.MiExcepcion;
+import compartidos.beans.excepciones.MiExcepcionLogica;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Mathias
+ */
+public interface ILogicaCliente {
+    
+    Cliente buscarCliente(long pCedula) throws ClassNotFoundException, SQLException, MiExcepcionLogica, MiExcepcion;
+    void altaCliente(Cliente pCliente) throws ClassNotFoundException, SQLException, MiExcepcionLogica, MiExcepcion;
+    void modificarCliente(Cliente pCliente) throws ClassNotFoundException, SQLException, MiExcepcionLogica, MiExcepcion;
+}
