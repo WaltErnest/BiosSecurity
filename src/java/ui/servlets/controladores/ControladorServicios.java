da72448e90eca7e46b2caa5ba819349376616dfe
Mathias <Mathias@192.168.1.102>
2017-07-08 17:02:36 -0300

    Agregado alta cliente

--- src/java/ui/servlets/controladores/ControladorServicios.java
+++ src/java/ui/servlets/controladores/ControladorServicios.java
@@ -28,8 +28,58 @@ public class ControladorServicios extends Controlador {
         mostrarVista("index", request, response);
     }
 
-    public void altaCliente_get (HttpServletRequest request, HttpServletResponse response) {
+    public void altacliente_get(HttpServletRequest request, HttpServletResponse response) {
             mostrarVista("altaCliente", request, response);
+    }
+    
+    public void altacliente_post(HttpServletRequest request, HttpServletResponse response) {
+        long cedula = 0;
+        
+        try {
+            cedula = Long.parseLong(request.getParameter("cedula"));            
+        } catch (NumberFormatException ex) {
+            cargarMensaje("¡ERROR! La cedula no es válida", request);
+            
+            mostrarVista("altaCliente", request, response);
+            
+            return;
+        }
+        
+        String nombre = request.getParameter("nombre");
+        
+        String direccionCobro = request.getParameter("direccionCobro");
+        
+        String barrioDirCobro = request.getParameter("barrioDirCobro");
+        
+        long telefono = 0;
+        
+        try {
+            telefono = Long.parseLong(request.getParameter("telefono"));
+        } catch (NumberFormatException ex) {
+            cargarMensaje("¡ERROR! El teléfono no es válido", request);
+            
+            mostrarVista("altaCliente", request, response);
+            
+            return;
+        }
+        
+        Cliente cliente = new Cliente(cedula, nombre, direccionCobro, barrioDirCobro, telefono);
+        
+        try {
+            FabricaLogica.GetLogicaCliente().altaCliente(cliente);
+            
+            cargarMensaje("¡El cliente fue agregado!", request.getSession());
+            
+            response.sendRedirect("servicios");
+        } catch (MiExcepcion ex) {
+            cargarMensaje("¡ERROR! " + ex.getMessage(), request);
+            
+            mostrarVista("altaCliente", request, response);
+        } catch (Exception ex) {
+            cargarMensaje("¡ERROR! Se produjo un error al agregar al cliente.", request);
+            
+            mostrarVista("altaCliente", request, response);
+        }
     }
 /*
     @Override
